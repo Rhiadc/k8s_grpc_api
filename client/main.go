4db8c828d6f8bc2af419ffa4b89e9cb3fe44c03b
Rhiadc <rhiad.ciccoli@gmail.com>
2022-06-18 18:55:36 -0300

    add gin

--- client/main.go
+++ client/main.go
@@ -1,18 +1,75 @@
 package main
 
 import (
-	"fmt"
+	"log"
+	"net/http"
+	"strconv"
 
-	"github.com/rhiadc/grpc_api/client/domain"
+	"github.com/gin-gonic/gin"
+	"github.com/rhiadc/grpc_api/client/proto"
+	"google.golang.org/grpc"
 )
 
 func main() {
 
-	user := &domain.User{
-		FirstName: "Badger",
-		LastName:  "Smith",
-		Email:     "Badger.Smith@gmail.com",
+	conn, err := grpc.Dial("localhost:4040", grpc.WithInsecure())
+
+	if err != nil {
+		panic(err)
 	}
 
-	fmt.Println(user.Validate())
+	client := proto.NewAddServiceClient(conn)
+
+	g := gin.Default()
+
+	g.GET("/add/:a/:b", func(ctx *gin.Context) {
+		a, err := strconv.ParseUint(ctx.Param("a"), 10, 64)
+		if err != nil {
+			ctx.JSON(http.StatusBadRequest, gin.H{"error": "Invalid parameter A"})
+			return
+		}
+
+		b, err := strconv.ParseUint(ctx.Param("b"), 10, 64)
+		if err != nil {
+			ctx.JSON(http.StatusBadRequest, gin.H{"error": "Invalid parameter B"})
+			return
+		}
+
+		req := &proto.Request{A: int64(a), B: int64(b)}
+
+		if response, err := client.Add(ctx, req); err == nil {
+			ctx.JSON(http.StatusOK, gin.H{"result": string(response.Result)})
+		} else {
+			ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		}
+
+	})
+
+	g.GET("/mult/:a/:b", func(ctx *gin.Context) {
+		a, err := strconv.ParseUint(ctx.Param("a"), 10, 64)
+		if err != nil {
+			ctx.JSON(http.StatusBadRequest, gin.H{"error": "Invalid parameter A"})
+			return
+		}
+
+		b, err := strconv.ParseUint(ctx.Param("b"), 10, 64)
+		if err != nil {
+			ctx.JSON(http.StatusBadRequest, gin.H{"error": "Invalid parameter B"})
+			return
+		}
+
+		req := &proto.Request{A: int64(a), B: int64(b)}
+
+		if response, err := client.Multiply(ctx, req); err == nil {
+			ctx.JSON(http.StatusOK, gin.H{"result": strconv.FormatInt(response.Result, 10)})
+		} else {
+			ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		}
+
+	})
+
+	if err := g.Run(":8080"); err != nil {
+		log.Fatalf("Failed to run server: %v", err)
+	}
+	//some comment
 }
